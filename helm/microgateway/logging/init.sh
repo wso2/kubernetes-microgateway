f5f21bc0d796f7eda4476e7538f32f5d776d0826
Aaquiff <aaquiff@wso2.com>
2019-08-31 16:23:32 +0530

    Fix incorrect log file name

--- helm/microgateway/logging/init.sh
+++ helm/microgateway/logging/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 touch /home/ballerina/logs/microgw.log
-/ballerina/runtime/bin/ballerina run --config /home/ballerina/conf/micro-gw.conf $PROJECT.balx 2>&1 | tee -a /home/ballerina/logs/microgw.logs
+/ballerina/runtime/bin/ballerina run --config /home/ballerina/conf/micro-gw.conf $PROJECT.balx 2>&1 | tee -a /home/ballerina/logs/microgw.log
